4cb1b67901d486bb65cc9d587340fb985bd19774
Georgis <georgis.ka@hotmail.com>
2018-08-26 00:42:43 +0300

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
 	def create
 
 		@article = Article.new(article_params)
-		@article.user = User.first # ensures that my article has a user
+		@article.user = current_user # ensures that my article has a user
 		if @article.save
 			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)
